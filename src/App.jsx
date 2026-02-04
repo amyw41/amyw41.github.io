f96e5cbf5381fd6f349044b66e15acf0ad976f9e
Amy Wang <amysonothere@gmail.com>
2026-02-03 23:25:07 -0500

    yay good version

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import './App.css'
 import CursorCircle from './components/CursorCircle'
 import Taskbar from './components/Taskbar'
@@ -6,12 +6,40 @@ import Home from './pages/Home'
 import Work from './pages/Work'
 import Me from './pages/Me'
 import Play from './pages/Play'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [isTaskbarVisible, setIsTaskbarVisible] = useState(true)
+  const [lastScrollY, setLastScrollY] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
+      
+      // Show taskbar when near top
+      if (currentScrollY < 50) {
+        setIsTaskbarVisible(true)
+      } 
+      // Hide taskbar when scrolling down
+      else if (currentScrollY > lastScrollY) {
+        setIsTaskbarVisible(false)
+      } 
+      // Show taskbar when scrolling up
+      else if (currentScrollY < lastScrollY) {
+        setIsTaskbarVisible(true)
+      }
+      
+      setLastScrollY(currentScrollY)
+    }
+
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [lastScrollY])
+
   return (
     <BrowserRouter>
       <CursorCircle />
-      <Taskbar />
+      <Taskbar isVisible={isTaskbarVisible} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/work" element={<Work />} />
--- src/components/Taskbar.jsx
+++ src/components/Taskbar.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 
-function Taskbar() {
+function Taskbar({ isVisible }) {
   return (
-    <nav className="taskbar">
+    <nav className={`taskbar ${isVisible ? 'visible' : 'hidden'}`}>
       <Link to="/" className="nav-left">home</Link>
       <span className="nav-right">
         <a href="/#work">work</a>
-        <Link to="/me">me</Link>
+        <Link to="/me">about</Link>
         <Link to="/play">play</Link>
       </span>
     </nav>
