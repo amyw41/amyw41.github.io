da9e0683abb4f475c6c392b11507a919dfdc5afd
Amy Wang <amysonothere@gmail.com>
2026-03-02 11:20:45 -0500

    add about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,13 @@ function About() {
   return (
     <section className="page-container">
       <h1 className="about-title">hi, I'm amy!</h1>
-      <p className="about-description">I'm a Management Engineering student at Waterloo!</p>
-      <p className="about-description">One of my favourite things to do is analyze regular, seemingly unimpressive things and find the beauty within. I began pursuing design when I realized it is exactly that.</p>
-      <p className="about-description">I'm currently exploring how systems, people, and aesthetics come together to shape realistic, everyday experiences.</p>
-      <p className="about-description">In my free time, I…<br />• Watch niche asian shows (kdrama fans hmu!)<br />• Listen to 3-hour-long video essays<br />• Drink too much matcha (and its not even the good kind…)</p>
+      <p className="about-description">I'm a Management Engineering student at Waterloo ࣪⊹ 𑣲₍ ᐢ. .ᐢ₎˖ °⋆</p>
+      <p className="about-description">One of my favourite things to do is analyze regular, seemingly unimpressive things to find the beauty within - which is why I design!</p>
+      <p className="about-description">I'm currently exploring how systems, people, and aesthetics come together to shape delightful experiences.</p>
+      <p className="about-description">In my free time, I…
+        <br />★ Dance! I'm currently re-learning ballet pointe
+        <br />★ Think about alternate Harry Potter and Hunger Games endings
+        <br />★ Listen to 3-hour-long video essays about books & pop culture<br /></p>
     </section>
   );
 }
